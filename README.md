01f064adc22d5657407c21204346f4eff0c2683c
Daniel Sullivan <ojojoj89@gmail.com>
2023-10-18 23:10:44 -0500

    formatting SVG and setting parameters for answers

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # SVG-Logo-Maker
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Credits:
+Talked with a friend, to help walk me through the logic, for times when I was lost, as well as help with code in a couple places where my  code wasn't functional.
+Source for await function, I learned about this previously, but had to reread to fully know how it functions.
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await 
--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const inquirer = require('inquirer');
-
-
+const inquirer = require('inquirer'); 
+const fs = require('fs');
+const { userInfo } = require('os');
+//importing the inquirer and fs modules
+// const {Square, Circle, Triangle} = require('./lib/shapes.test.js'); // NEEDS TO BE CHANGED
+//list of the questions that the user will be asked
 const questions = [
     {
         type: "input",
@@ -24,3 +27,45 @@ const questions = [
         choices: ["Square", "Circle", "Triangle"]
     }
 ];
+    //class for the shapes that will be used in the logo, included in the SVG
+    class SVG {
+        constructor() {
+            this.textElement ='';
+            this.shapeElement ='';
+        }
+        render() { 
+            //setting the text and shape elements to be rendered in the SVG
+         return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shapeElement}${this.textElement}</svg>`;
+        }
+        setText(text, color){
+            this.textElement = '<text x="150" y="125" font-size="60" text-anchor="middle" fill="' + color + '">' + text + '</text>';
+        }
+        setShape(shape){
+            this.shapeElement = shape.render();
+        }
+    }
+
+    // writeToFile function to write the answers to the file, confirming that the file has been written, or throwing an error if it has not
+    function writeToFile(fileName, answers) {
+        console.log("Transfering answers to file now!")
+        fs.writeFile(fileName, answers, (err) => {
+            if (err) throw err;
+            console.log("File saved! Congratulations! You have generated your new logo!");
+        });
+    }
+    //prompts the user with the questions, and then takes the answers and writes them to the file
+    const answers = await inquirer.prompt(questions);
+    // if the user enters a valid three letter abbreviation, the program will continue, otherwise it will throw an error
+    if (answers.logo-text.length===3){
+        userTextInput = answers.logo-text;
+    }   else {
+        console.log("Please enter a valid three letter abbreviation for your company!");
+        return;
+    }
+    console.log(`User's text input: ${userTextInput}`);
+    userTextColor = answers.text-color;
+    console.log(`User's text color: ${userTextColor}`);
+    userShapeColor = answers.shape-color;
+    console.log(`User's shape color: ${userShapeColor}`);
+    userShape = answers.shape;
+    console.log(`User's shape: ${userShape}`);
